1be20f1a67d9f1d9167765aa59d732d39f407334
nullThink <legoking43@gmail.com>
2022-12-04 16:14:51 -0500

    Holiday helper start, image gallery functionality

---
+++ main.js
@@ -0,0 +1,55 @@
+var imageArray = new Array();
+imageArray[0] = new Image();
+imageArray[0].src = "images/airpurifier.jpg";
+
+imageArray[1] = new Image();
+imageArray[1].src = "images/badmintonset1.jpg";
+
+imageArray[2] = new Image();
+imageArray[2].src = "images/beard_trimmer.jpg";
+
+imageArray[3] = new Image();
+imageArray[3].src = "images/BobaBlushie.jpg";
+
+
+imageArray[4] = new Image();
+imageArray[4].src = "images/bouquetKit.jpg";
+
+imageArray[5] = new Image();
+imageArray[5].src = "images/cutecatlamp.jpg";
+
+imageArray[6] = new Image();
+imageArray[6].src = "images/dysonairwrap.jpg";
+
+imageArray[7] = new Image();
+imageArray[7].src = "images/hotcocoakit.jpg";
+
+imageArray[8] = new Image();
+imageArray[8].src = "images/knitsocks.jpg";
+
+imageArray[9] = new Image();
+imageArray[9].src = "images/prismacolor_pencils.jpg";
+
+
+imageArray[10] = new Image();
+imageArray[10].src = "images/remarkable tablet.jpg";
+
+
+imageArray[11] = new Image();
+imageArray[11].src = "images/sony_headphones2.jpg";
+
+imageArray[12] = new Image();
+imageArray[12].src = "images/sushi_making_kit.jpg";
+
+imageArray[13] = new Image();
+imageArray[13].src = "images/toasteroven.jpg";
+
+const gallery = document.getElementById("currentImage");
+let toggle = true;
+gallery.addEventListener('click', changePicture() {
+    toggle = !toggle;
+    gallery.src
+
+
+
+})
